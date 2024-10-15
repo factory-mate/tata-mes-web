3dff373cddc63db6e02ed4f3f371726f6c12b2db
Bruce <recall4056@gmail.com>
2024-10-15 15:27:12 +0800

    #2594

--- src/views/personalCenter/memAdmin/memAdminEdit.vue
+++ src/views/personalCenter/memAdmin/memAdminEdit.vue
@@ -166,9 +166,12 @@ onActivated(() => {
   // if(initType.value){
   //     getAddUser(Route.meta.ModelCode)
   // }
-  if (rowId.value != Route.params.rowId) {
-    getAddUser(Route.meta.ModelCode);
-  }
+  // if (rowId.value != Route.params.rowId) {
+  //   getAddUser(Route.meta.ModelCode);
+  // }
+
+  getAddUser(Route.meta.ModelCode);
+
   rowId.value = Route.params.rowId;
   initType.value = false;
   if (history.state.row) {
