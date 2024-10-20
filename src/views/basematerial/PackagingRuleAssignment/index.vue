3b2ad9bf03e4283fd54a885d3a4363dab4fccb1c
Bruce <recall4056@gmail.com>
2024-10-20 11:26:41 +0800

    #2598

--- src/views/basematerial/PackagingRuleAssignment/index.vue
+++ src/views/basematerial/PackagingRuleAssignment/index.vue
@@ -97,6 +97,7 @@
         v-model:page="queryParams.PageIndex"
         v-model:limit="queryParams.PageSize"
         @pagination="changPage"
+        :page-sizes="[20, 50, 100]"
       />
     </el-card>
     <!-- 弹窗 -->
@@ -216,7 +217,7 @@ const getData: any = async (val: string) => {
 //分页查询参数
 const queryParams = reactive({
   PageIndex: 1,
-  PageSize: 10
+  PageSize: 20
 });
 //总条数
 const total = ref(0);
@@ -539,7 +540,7 @@ const resetForm = (val: any) => {
   OrderByFileds.value = '';
   tableColumns.value = tableSortInit(tableColumns.value);
   queryParams.PageIndex = 1;
-  queryParams.PageSize = 10;
+  queryParams.PageSize = 20;
   tableAxios();
 };
 
