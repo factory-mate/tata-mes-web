f030627c39f22e43d46e9862509eb0ae1cb5ddc2
Bruce <recall4056@gmail.com>
2024-10-24 11:21:41 +0800

    fix: bug

--- src/components/ViewFormHeard/index.vue
+++ src/components/ViewFormHeard/index.vue
@@ -2355,7 +2355,8 @@ const newAdd = () => {
     dataValue = { cLableCodes: arr };
   } else if (Route.name == 'InventoryListAdd') {
     dataValue = {
-      body: validateObj.value.tableData
+      body: validateObj.value.tableData,
+      cVouchTypeCode: '1'
     };
   } else {
     dataValue = { uNITCONVERTs: ButObjTableData.value };
