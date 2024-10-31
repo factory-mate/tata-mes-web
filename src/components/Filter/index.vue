cd3f7288fa2b8acd5b3f7d8a1897e199c00064ec
Bruce <recall4056@gmail.com>
2024-11-01 03:51:03 +0800

    #2756

--- src/components/Filter/index.vue
+++ src/components/Filter/index.vue
@@ -426,7 +426,7 @@ const selectData = (val: any) => {
       if (titleName.value === item.Resource.cAttributeName) {
         if (Route.name === 'InventoryList') {
           if (item.Resource.cAttributeCode === 'cDefindParm02') {
-            item.cAttributeCodeValue = val.value[0].cWareHouseAreaCode;
+            item.cAttributeCodeValue = val.value[0].cWareHouseAreaName;
           }
         }
 
