1e3541db65d3d2c45b20e8881edb53179434b9b4
Bruce <recall4056@gmail.com>
2025-03-12 21:07:49 +0800

    #3247

--- src/views/ProductionReturn/ReturnQuality/index.vue
+++ src/views/ProductionReturn/ReturnQuality/index.vue
@@ -32,7 +32,7 @@
           <el-table-column
             label="操作"
             fixed="right"
-            width="200px"
+            width="260px"
             align="center"
           >
             <template #header>
