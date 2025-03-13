5afba6c1615b24c656e190b92bd3ea22844a96b5
Bruce <recall4056@gmail.com>
2025-03-13 21:03:50 +0800

    #3256

--- src/views/WMSSystem/PurchaseNote/AddPurchaseNoteEdit.vue
+++ src/views/WMSSystem/PurchaseNote/AddPurchaseNoteEdit.vue
@@ -98,6 +98,9 @@
         @handleSelectionChange="ThandleSelectionChange"
         :disabledHide="false"
         max-height="500"
+        :setWidth="setWidth"
+        custom-width
+        :show-index="false"
       >
       </myTable>
       <template #footer>
@@ -595,6 +598,26 @@ const clickEdit = (obj: any) => {
   disabled.value = false;
   disa.value = false;
   $bus.emit('TabTitleVal', { name: Route.name, title: '采购单编辑' });
+};
+
+const setWidth = row => {
+  switch (row.label) {
+    case '申请单号':
+      return 180;
+    case '物料编码':
+      return 120;
+    case '物料名称':
+      return 200;
+    case '规格型号':
+      return 120;
+    case '数量':
+    case '单位':
+      return 80;
+    case '交货日期':
+      return 120;
+    default:
+      return 200;
+  }
 };
 </script>
 
