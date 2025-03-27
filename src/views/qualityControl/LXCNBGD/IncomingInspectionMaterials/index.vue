8e9431231a092e0a524dd859b36dce9ff68c84c9
Bruce <recall4056@gmail.com>
2025-03-28 05:06:42 +0900

    #3422

--- src/views/qualityControl/LXCNBGD/IncomingInspectionMaterials/index.vue
+++ src/views/qualityControl/LXCNBGD/IncomingInspectionMaterials/index.vue
@@ -498,7 +498,7 @@ const data = reactive({
   dialogV: false,
   dialogTitle: '编辑',
   Conditions: '',
-  OrderByFileds: ''
+  OrderByFileds: 'cCode'
 });
 const { dialogV, dialogTitle, Conditions, OrderByFileds } = toRefs(data);
 // 搜索
@@ -511,7 +511,7 @@ const ClickSearch = (val: any) => {
 // 重置
 const resetForm = (val: any) => {
   Conditions.value = '';
-  OrderByFileds.value = '';
+  OrderByFileds.value = 'cCode';
   tableColumns.value = tableSortInit(tableColumns.value);
   queryParams.PageIndex = 1;
   queryParams.PageSize = 20;
