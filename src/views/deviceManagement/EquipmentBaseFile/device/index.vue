722784a065fd931425b695028aba595590f86519
Bruce <recall4056@gmail.com>
2025-04-11 12:01:43 +0800

    #3473

--- src/views/deviceManagement/EquipmentBaseFile/device/index.vue
+++ src/views/deviceManagement/EquipmentBaseFile/device/index.vue
@@ -674,7 +674,7 @@ const data = reactive({
   dialogV: false,
   dialogTitle: '编辑',
   Conditions: '',
-  OrderByFileds: ''
+  OrderByFileds: 'cDeviceName'
 });
 const { Conditions, OrderByFileds } = toRefs(data);
 // 搜索
@@ -686,7 +686,7 @@ const ClickSearch = (val: any) => {
 // 重置
 const resetForm = (val: any) => {
   Conditions.value = '';
-  OrderByFileds.value = '';
+  OrderByFileds.value = 'cDeviceName';
   tableColumns.value = tableSortInit(tableColumns.value);
   queryParams.PageIndex = 1;
   queryParams.PageSize = 20;
