5c5d1d6ffc4d704299356ff66f35ee2b92036e71
Bruce <recall4056@gmail.com>
2025-05-14 13:46:40 +0800

    #3628

--- src/views/ReoprtForms/WareHouseTable/LogCollectStat/index.vue
+++ src/views/ReoprtForms/WareHouseTable/LogCollectStat/index.vue
@@ -389,7 +389,7 @@ const data = reactive({
   dialogV: false,
   dialogTitle: '编辑',
   Conditions: '',
-  OrderByFileds: ''
+  OrderByFileds: 'dDate desc,cLogistics'
 });
 const { Conditions, OrderByFileds } = toRefs(data);
 // 搜索
@@ -402,7 +402,7 @@ const ClickSearch = (val: any) => {
 // 重置
 const resetForm = (val: any) => {
   Conditions.value = '';
-  OrderByFileds.value = '';
+  OrderByFileds.value = 'dDate desc,cLogistics';
   tableColumns.value = tableSortInit(tableColumns.value);
   queryParams.PageIndex = 1;
   queryParams.PageSize = 20;
