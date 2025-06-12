68134ba01743f0871400b7cccc6cfc6b54c1f9d4
Bruce <recall4056@gmail.com>
2025-06-12 08:25:19 +0800

    #3772

--- src/views/WMSSystem/applicationInspection/index.vue
+++ src/views/WMSSystem/applicationInspection/index.vue
@@ -148,19 +148,21 @@ const selList = ref([]) as any;
 
 const initType = ref(true);
 onActivated(() => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  let val = window.sessionStorage.getItem('clickSider')
-    ? JSON.parse(window.sessionStorage.getItem('clickSider'))
-    : '';
-  if (val == Route.name) {
+  if (Route.name === 'applicationInspection') {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    let val = window.sessionStorage.getItem('clickSider')
+      ? JSON.parse(window.sessionStorage.getItem('clickSider'))
+      : '';
+    if (val == Route.name) {
+      initType.value = false;
+      getData(Route.meta.ModelCode);
+    }
+    if (initType.value) {
+      getData(Route.meta.ModelCode);
+    }
     initType.value = false;
-    getData(Route.meta.ModelCode);
   }
-  if (initType.value) {
-    getData(Route.meta.ModelCode);
-  }
-  initType.value = false;
 });
 // 新增/编辑后的刷新
 $bus.on('tableUpData', (v: any) => {
