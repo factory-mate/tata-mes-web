ac7cd0212c41da8524ae4b7f2e2ec8937b2880a6
Bruce <recall4056@gmail.com>
2025-08-25 11:24:44 +0800

    #4019

--- src/views/WMSSystem/PurchaseRequestNoProd/index.vue
+++ src/views/WMSSystem/PurchaseRequestNoProd/index.vue
@@ -48,14 +48,14 @@
                 v-for="item in tableButton"
                 :key="item.Resource.cAttributeName"
               >
-                <!-- 0: 保存，1：未审核，5:驳回，10:已审核，15：已采购 -->
+                <!-- 0: 保存，51:驳回-->
 
                 <el-button
                   v-if="
                     (item.Resource.cAttributeCode === 'Edit' &&
-                      ![5].includes(scope.row.iStatus)) ||
+                      [0, 51].includes(scope.row.iStatus)) ||
                     (item.Resource.cAttributeCode === 'Delete' &&
-                      ![5].includes(scope.row.iStatus)) ||
+                      [0, 51].includes(scope.row.iStatus)) ||
                     !['Edit', 'Delete'].includes(item.Resource.cAttributeCode)
                   "
                   type="primary"
