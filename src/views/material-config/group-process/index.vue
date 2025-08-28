b693f4978c7b1fb9f507dd75213136d77f83e380
Bruce <recall4056@gmail.com>
2025-08-29 06:15:28 +0800

    #3947

--- src/views/material-config/group-process/index.vue
+++ src/views/material-config/group-process/index.vue
@@ -535,7 +535,7 @@ const ExportAll = async (obj: any) => {
     }
   };
   ElLoading.service({ lock: true, text: '加载中.....' });
-  exportAnalysisHooks(data, '物料供应商对照-所有');
+  exportAnalysisHooks(data, '物料分组与工序对照-所有');
   ElLoading.service().close();
 };
 //按钮导出当前页
@@ -551,7 +551,7 @@ const ExportOne = async (obj: any) => {
     }
   };
   ElLoading.service({ lock: true, text: '加载中.....' });
-  exportAnalysisHooks(data, '物料供应商对照');
+  exportAnalysisHooks(data, '物料分组与工序对照');
   ElLoading.service().close();
 };
 
