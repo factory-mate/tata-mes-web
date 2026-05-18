bb193959895adcb31cd7d34c07cadeeaa8898d06
Bruce <recall4056@gmail.com>
2026-05-18 10:50:24 +0800

    fix: bug

--- src/views/qualityControl/LXCNBGD/ProductProcessCheck/index.vue
+++ src/views/qualityControl/LXCNBGD/ProductProcessCheck/index.vue
@@ -502,7 +502,7 @@ const data = reactive({
   dialogV: false,
   dialogTitle: '编辑',
   Conditions: '',
-  OrderByFileds: ''
+  OrderByFileds: 'dDate desc'
 });
 const { dialogV, dialogTitle, Conditions, OrderByFileds } = toRefs(data);
 // 搜索
@@ -515,7 +515,7 @@ const ClickSearch = (val: any) => {
 // 重置
 const resetForm = (val: any) => {
   Conditions.value = '';
-  OrderByFileds.value = '';
+  OrderByFileds.value = 'dDate desc';
   tableColumns.value = tableSortInit(tableColumns.value);
   queryParams.PageIndex = 1;
   queryParams.PageSize = 20;
